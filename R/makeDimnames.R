ce341c9114c32ddc81aba17acf09ecd79ac33275
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-26 09:36:07 -0500

    Initial commit

---
+++ R/makeDimnames.R
@@ -0,0 +1,12 @@
+makeDimnames <- function(object) {
+    assert(hasDimnames(object))
+    # Row names.
+    if (hasRownames(object)) {
+        rownames(object) <- makeNames(rownames(object), unique = TRUE)
+    }
+    # Column names.
+    if (hasColnames(object)) {
+        colnames(object) <- makeNames(colnames(object), unique = TRUE)
+    }
+    object
+}
---
+++ R/upperCamel-methods.R
@@ -0,0 +1,179 @@
+#' @name upperCamel
+#' @inherit bioverbs::upperCamel
+NULL
+
+
+
+#' @importFrom bioverbs upperCamel
+#' @aliases NULL
+#' @export
+bioverbs::upperCamel
+
+
+
+.upperCamel <-  # nolint
+    function(object, strict = FALSE) {
+        camel(object, format = "upper", strict = strict)
+    }
+
+
+
+upperCamel.ANY <-  # nolint
+    function(object, strict = FALSE) {
+        if (hasNames(object)) {
+            names(object) <- upperCamel(names(object), strict = strict)
+        }
+        object
+    }
+
+
+
+#' @rdname upperCamel
+#' @export
+setMethod(
+    f = "upperCamel",
+    signature = signature("ANY"),
+    definition = upperCamel.ANY
+)
+
+
+
+upperCamel.character <-  # nolint
+    function(object, strict = FALSE) {
+        if (hasNames(object)) {
+            names <- .upperCamel(names(object), strict = strict)
+        } else {
+            names <- NULL
+        }
+        object <- .upperCamel(object, strict = strict)
+        names(object) <- names
+        object
+    }
+
+
+
+#' @rdname upperCamel
+#' @export
+setMethod(
+    f = "upperCamel",
+    signature = signature("character"),
+    definition = upperCamel.character
+)
+
+
+
+upperCamel.factor <-  # nolint
+    function(object, strict = FALSE) {
+        names <- names(object)
+        object <- object %>%
+            as.character() %>%
+            upperCamel(strict = strict) %>%
+            as.factor()
+        names(object) <- upperCamel(names, strict = strict)
+        object
+    }
+
+
+
+#' @rdname upperCamel
+#' @export
+setMethod(
+    f = "upperCamel",
+    signature = signature("factor"),
+    definition = upperCamel.factor
+)
+
+
+
+upperCamel.matrix <-  # nolint
+    function(
+        object,
+        rownames = FALSE,
+        colnames = TRUE,
+        strict = FALSE
+    ) {
+        assert(
+            hasDimnames(object),
+            isFlag(rownames)
+        )
+        if (isTRUE(rownames) && hasRownames(object)) {
+            rownames(object) <-
+                upperCamel(rownames(object), strict = strict)
+        }
+        if (isTRUE(colnames) && hasColnames(object)) {
+            colnames(object) <-
+                upperCamel(colnames(object), strict = strict)
+        }
+        object
+    }
+
+
+
+#' @rdname upperCamel
+#' @export
+setMethod(
+    f = "upperCamel",
+    signature = signature("matrix"),
+    definition = upperCamel.matrix
+)
+
+
+
+upperCamel.data.frame <- upperCamel.matrix  # nolint
+
+
+
+#' @rdname upperCamel
+#' @export
+setMethod(
+    f = "upperCamel",
+    signature = signature("data.frame"),
+    definition = upperCamel.data.frame
+)
+
+
+
+upperCamel.DataFrame <- upperCamel.data.frame  # nolint
+
+
+
+#' @rdname upperCamel
+#' @export
+setMethod(
+    f = "upperCamel",
+    signature = signature("DataFrame"),
+    definition = upperCamel.DataFrame
+)
+
+
+
+upperCamel.GRanges <-  # nolint
+    function(object, strict = FALSE) {
+        colnames(mcols(object)) <-
+            upperCamel(colnames(mcols(object)), strict = strict)
+        object
+    }
+
+
+
+#' @rdname upperCamel
+#' @export
+setMethod(
+    f = "upperCamel",
+    signature = signature("GRanges"),
+    definition = upperCamel.GRanges
+)
+
+
+
+upperCamel.GRangesList <- upperCamel.GRanges  # nolint
+
+
+
+#' @rdname upperCamel
+#' @export
+setMethod(
+    f = "upperCamel",
+    signature = signature("GRangesList"),
+    definition = upperCamel.GRangesList
+)
