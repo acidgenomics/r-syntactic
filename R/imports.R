8e8544b99cf632562f9b6ff95aa4baed277d007f
Michael Steinbaugh <mike@steinbaugh.com>
2019-01-23 05:46:51 -0500

    Add pkgdown website

---
+++ R/imports.R
@@ -0,0 +1,11 @@
+#' @importClassesFrom GenomicRanges GRanges GRangesList
+#' @importClassesFrom Matrix Matrix
+#' @importClassesFrom S4Vectors DataFrame
+#'
+#' @importFrom S4Vectors mcols mcols<-
+#' @importFrom goalie assert hasColnames hasDimnames hasNames hasRownames
+#'   isCharacter isFlag
+#' @importFrom magrittr %>%
+#' @importFrom methods setMethod signature
+#' @importFrom utils globalVariables
+NULL
