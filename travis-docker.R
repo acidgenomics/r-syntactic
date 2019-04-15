e926aaa30252a495fff17b731554abc76598a2f6
Michael Steinbaugh <mike@steinbaugh.com>
2019-04-15 08:58:21 -0400

    Add covr and lintr checks

--- travis-docker.R
+++ travis-docker.R
@@ -1,2 +1,4 @@
-rcmdcheck::rcmdcheck(path = ".", args = "--no-manual")
-BiocCheck::BiocCheck(package = ".")
+rcmdcheck::rcmdcheck(args = "--no-manual")
+BiocCheck::BiocCheck(`quit-with-status` = TRUE)
+lintr::lint_package()
+covr::report()
