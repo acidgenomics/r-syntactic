7854caac9718cce08eb43d54d3e0f679b57eccfb
Michael Steinbaugh <mike@steinbaugh.com>
2019-07-19 16:04:08 -0400

    Prepare v0.2 release

--- R/params.R
+++ R/params.R
@@ -6,6 +6,12 @@
 #' @param colnames `logical(1)`.
 #'   Apply sanitization on column names. This is generally recommended by
 #'   default.
+#' @param mcols `logical(1)`.
+#'   FIXME
+#' @param metadata `logical(1)`.
+#'   FIXME
+#' @param names `logical(1)`.
+#'   Sanitize names.
 #' @param object Object.
 #' @param rownames `logical(1)`.
 #'   Apply sanitization on row names. This is not generally recommended by
