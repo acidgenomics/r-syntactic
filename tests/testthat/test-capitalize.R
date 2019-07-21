d479111750258f23bb51db0bac1e3c02499422ef
Michael Steinbaugh <mike@steinbaugh.com>
2019-07-21 14:05:06 -0400

    Update unit tests

--- tests/testthat/test-capitalize.R
+++ tests/testthat/test-capitalize.R
@@ -4,18 +4,19 @@ test_that("capitalize", {
     expect_identical(
         object = capitalize(vec),
         expected = c(
+            "%GC",
+            "10uM",
+            "5'-3' bias",
+            "5prime",
+            "G2M.Score",
             "Hello world",
             "HELLO WORLD",
-            "RNAi clones",
+            "Mazda RX4",
             "NCount",
+            "RNAi clones",
             "Tx2gene",
             "TX2GeneID",
-            "G2M.Score",
             "WorfdbHTMLRemap",
-            "Mazda RX4",
-            "%GC",
-            "5prime",
-            "5'-3' bias",
             "123",
             NA_character_
         )
