8e8f641aa3014b3b95ce8c38dd86ddc702f70446
Michael Steinbaugh <mike@steinbaugh.com>
2019-07-21 13:00:04 -0400

    Update working example

--- R/mcolnames-methods.R
+++ R/mcolnames-methods.R
@@ -5,7 +5,9 @@
 #' @param ... Additional arguments.
 #'
 #' @examples
-#' data(rse, package = "acidtest")
+#' data(RangedSummarizedExperiment, package = "acidtest")
+#' rse <- RangedSummarizedExperiment
+#'
 #' mcolnames(rse)
 #'
 #' ## Asignment method.
