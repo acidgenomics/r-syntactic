14616439df4de878f5872bb5d26c557df6b48f7a
Michael Steinbaugh <mike@steinbaugh.com>
2019-10-21 17:42:46 -0400

    Draft update

--- R/capitalize-methods.R
+++ R/capitalize-methods.R
@@ -10,32 +10,3 @@
 #' @examples
 #' capitalize(c("hello", "world"))
 NULL
-
-
-
-## Using `vapply()` call here to preserve `NA_character_`.
-`capitalize,character` <-  # nolint
-    function(object) {
-        vapply(
-            X = object,
-            FUN = function(x) {
-                if (is.na(x)) return(x)
-                first <- substring(x, first = 1L, last = 1L)
-                first <- toupper(first)
-                tail <- substring(x, first = 2L)
-                paste(first, tail, sep = "")
-            },
-            FUN.VALUE = character(1L),
-            USE.NAMES = FALSE
-        )
-    }
-
-
-
-#' @rdname capitalize
-#' @export
-setMethod(
-    f = "capitalize",
-    signature = signature("character"),
-    definition = `capitalize,character`
-)
