22f058475844fe320414a9b64ceef3986e01e9ba
Michael Steinbaugh <mike@steinbaugh.com>
2019-10-21 22:41:32 -0400

    Convert all functions to S4 methods

--- R/rename-internal.R
+++
@@ -1,9 +0,0 @@
-## FIXME Need a smart approach to file extensions.
-## FIXME Need to support either .txt OR .txt.gz
-## Compressed file extensions...
-
-
-
-## isFileSystemCaseSensitive
-.rename <- function(from, to) {
-}
