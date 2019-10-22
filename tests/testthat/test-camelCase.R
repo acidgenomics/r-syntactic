6fd0cb1ea69eda08b9f30a0286625384a37c535d
Michael Steinbaugh <mike@steinbaugh.com>
2019-10-22 14:42:15 -0400

    Add code coverage for file rename mode

--- tests/testthat/test-camelCase.R
+++ tests/testthat/test-camelCase.R
@@ -98,3 +98,27 @@ test_that("Disable X prefix", {
         expected = c("x1" = "1FooBar")
     )
 })
+
+test_that("File rename mode", {
+    topdir <- "XXX"
+    unlink(topdir, recursive = TRUE)
+    dirs <- file.path(topdir, c("aaa-bbb", "ccc-ddd"))
+    files <- file.path(topdir, c("1-sample-A.fastq.gz", "hello-world.txt"))
+    input <- c(files, dirs)
+    lapply(dirs, dir.create, recursive = TRUE)
+    file.create(files)
+    output <- camelCase(input, rename = TRUE, prefix = FALSE)
+    expect_identical(
+        object = output,
+        expected = file.path(
+            topdir,
+            c(
+                "1SampleA.fastq.gz",
+                "helloWorld.txt",
+                "aaaBbb",
+                "cccDdd"
+            )
+        )
+    )
+    unlink(topdir, recursive = TRUE)
+})
