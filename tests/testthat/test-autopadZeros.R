ecf3700600318060f01132bc109d0d72ba080be6
Michael Steinbaugh <mike@steinbaugh.com>
2020-06-15 16:44:44 -0400

    Improve code coverage of left side padding

--- tests/testthat/test-autopadZeros.R
+++ tests/testthat/test-autopadZeros.R
@@ -5,13 +5,19 @@ test_that("integer", {
         object = autopadZeros(c(a = 1L, b = 10L, c = 100L)),
         expected = c(a = "001", b = "010", c = "100")
     )
+    expect_identical(
+        object = autopadZeros(c(a = 1L, b = 2L, c = 3L)),
+        expected = c(a = "1", b = "2", c = "3")
+    )
 })
 
 test_that("character", {
+    ## Left side.
     expect_identical(
-        object = autopadZeros(c(a = "A1", b = "B10")),
-        expected = c(a = "A01", b = "B10")
+        object = autopadZeros(c(a = "1-EV", b = "10-EV", c = "100-EV")),
+        expected = c(a = "001-EV", b = "010-EV", c = "100-EV")
     )
+    ## Right side.
     expect_identical(
         object = autopadZeros(c(a = "A1", b = "B10", c = "C100")),
         expected = c(a = "A001", b = "B010", c = "C100")
