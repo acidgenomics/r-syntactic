792aa5911a696d2c7c447c8a1215932a4c7b623b
Michael Steinbaugh <mike@steinbaugh.com>
2021-08-20 14:25:46 -0400

    Consolidate files

--- R/imports.R
+++
@@ -1,8 +0,0 @@
-#' @importFrom AcidBase basenameSansExt fileDepth fileExt printString realpath
-#' @importFrom goalie allHaveAccess allAreMatchingRegex assert hasColnames
-#'   hasDimnames hasNames hasNoDuplicates hasRownames isCharacter isDirectory
-#'   isFileSystemCaseSensitive isFlag isMatchingRegex isString
-#' @importFrom methods setMethod signature
-#' @importFrom stringi stri_trans_general
-#' @importFrom stringr str_length str_match str_pad
-NULL
