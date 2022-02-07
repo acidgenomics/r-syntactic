126e58c000000108500183499e24020434631ed1
Michael Steinbaugh <mike@steinbaugh.com>
2022-02-07 12:55:07 -0500

    Reorganize documentation

---
+++ R/imports.R
@@ -0,0 +1,19 @@
+## S4 generics =================================================================
+#' @importFrom AcidGenerics autopadZeros camelCase capitalize dottedCase
+#'   kebabCase makeDimnames makeLabel makeNames makeTitle makeWords sentenceCase
+#'   snakeCase upperCamelCase
+NULL
+
+
+
+## Standard functions ==========================================================
+#' @importFrom AcidBase basenameSansExt fileDepth fileExt printString realpath
+#' @importFrom AcidCLI abort alert alertInfo
+#' @importFrom goalie allHaveAccess allAreMatchingRegex assert hasColnames
+#'   hasDimnames hasNames hasNoDuplicates hasRownames isCharacter isDirectory
+#'   isFileSystemCaseSensitive isFlag isMatchingRegex isString isSubset
+#' @importFrom methods setMethod signature
+#' @importFrom stringi stri_trans_general
+#' @importFrom stringr str_length str_match str_pad
+#' @importFrom utils packageName
+NULL
