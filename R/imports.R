95d28fdd16561a17c65ec68da994a96d99d307ca
Michael Steinbaugh <mike@steinbaugh.com>
2022-04-29 14:17:21 -0400

    Work toward making the package lighter

--- R/imports.R
+++ R/imports.R
@@ -8,12 +8,9 @@ NULL
 
 ## Standard functions ==========================================================
 #' @importFrom AcidBase basenameSansExt fileDepth fileExt printString realpath
-#' @importFrom AcidCLI abort alert alertInfo
 #' @importFrom goalie allHaveAccess allAreMatchingRegex assert hasColnames
 #' hasDimnames hasNames hasNoDuplicates hasRownames isCharacter isDirectory
-#' isFileSystemCaseSensitive isFlag isMatchingRegex isString isSubset
+#' isFileSystemCaseSensitive isFlag isInstalled isMatchingRegex isString
+#' isSubset
 #' @importFrom methods setMethod signature
-#' @importFrom stringi stri_trans_general
-#' @importFrom stringr str_length str_match str_pad
-#' @importFrom utils packageName
 NULL
