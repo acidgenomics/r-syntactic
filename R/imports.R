5bea7f857014afdee72ec250fd999136aaf4b76b
Michael Steinbaugh <mike@steinbaugh.com>
2022-06-08 13:43:11 -0400

    Add line breaks

--- R/imports.R
+++ R/imports.R
@@ -1,4 +1,5 @@
 ## S4 generics =================================================================
+
 #' @importFrom AcidGenerics autopadZeros camelCase capitalize dottedCase
 #' kebabCase makeDimnames makeLabel makeNames makeTitle makeWords sentenceCase
 #' snakeCase upperCamelCase
@@ -7,6 +8,7 @@ NULL
 
 
 ## Standard functions ==========================================================
+
 #' @importFrom goalie allHaveAccess allAreMatchingRegex assert hasColnames
 #' hasDimnames hasNames hasNoDuplicates hasRownames isCharacter isDirectory
 #' isFileSystemCaseSensitive isFlag isInstalled isMatchingRegex isString
