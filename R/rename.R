4cb6eba1216f5670c51f3ae5548a906dbecc3d1d
Michael Steinbaugh <mike@steinbaugh.com>
2023-02-06 15:59:34 -0500

    Rework our renamer

--- R/rename.R
+++ R/rename.R
@@ -22,6 +22,9 @@
 #' @param recursive `logical(1)`.
 #' Should the function recurse into directories?
 #'
+#' @param dryRun `logical(1)`.
+#' Return the proposed file path modifications without modification.
+#'
 #' @return `character`.
 #' Renamed file paths.
 #'
@@ -42,30 +45,29 @@ syntacticRename <-
                  "camelCase",
                  "upperCamelCase"
              ),
-             quiet = FALSE) {
+             quiet = FALSE,
+             dryRun = FALSE) {
         assert(
             requireNamespace("AcidBase", quietly = TRUE),
             allHaveAccess(path),
             isFlag(recursive),
-            isFlag(quiet)
+            isFlag(quiet),
+            isFlag(dryRun)
         )
+        if (isTRUE(dryRun)) {
+            assert(isFALSE(quiet))
+        }
         if (isFALSE(quiet)) {
             assert(requireNamespace("AcidCLI", quietly = TRUE))
         }
         fun <- match.arg(fun)
-        what <- get(
-            x = fun,
-            envir = asNamespace("AcidGenerics"),
-            inherits = FALSE
-        )
-        assert(is.function(what))
         ## Shared arguments passed per file to syntactic naming function.
-        args <- list(
+        whatArgs <- list(
             "prefix" = FALSE,
             "smart" = TRUE
         )
         if (isSubset(fun, c("camelCase", "upperCamelCase"))) {
-            args[["strict"]] <- TRUE
+            whatArgs[["strict"]] <- TRUE
             lower <- FALSE
         } else {
             lower <- TRUE
@@ -76,81 +78,79 @@ syntacticRename <-
         } else {
             from <- AcidBase::realpath(path)
         }
-        insensitive <- !isTRUE(isFileSystemCaseSensitive())
+        toPath <- function(from, what, whatArgs, lower, quiet) {
+            dn <- dirname(from)
+            ext <- AcidBase::fileExt(from)
+            stem <- AcidBase::basenameSansExt(from)
+            if (isFALSE(grepl(pattern = "^[A-Za-z0-9]", x = stem))) {
+                if (isFALSE(quiet)) {
+                    AcidCLI::alertInfo(sprintf("Skipping {.file %s}.", to))
+                }
+                return(from)
+            }
+            if (isTRUE(lower)) {
+                stem <- tolower(stem)
+            }
+            whatArgs[["object"]] <- stem
+            stem <- do.call(what = what, args = whatArgs)
+            if (!is.na(ext)) {
+                bn <- paste0(stem, ".", ext)
+            } else {
+                bn <- stem
+            }
+            file.path(dn, bn)
+        }
         to <- vapply(
             X = from,
-            what = what,
+            what = get(
+                x = fun,
+                envir = asNamespace("AcidGenerics"),
+                inherits = FALSE
+            ),
             args = args,
-            insensitive = insensitive,
             lower = lower,
             quiet = quiet,
-            FUN = function(from,
-                           what,
-                           args,
-                           insensitive,
-                           lower,
-                           quiet) {
-                dir <- dirname(from)
-                ext <- AcidBase::fileExt(from)
-                stem <- AcidBase::basenameSansExt(from)
-                if (isFALSE(grepl(pattern = "^[A-Za-z0-9]", x = stem))) {
-                    if (isFALSE(quiet)) {
-                        AcidCLI::alertInfo(sprintf(
-                            "Skipping {.file %s}.",
-                            gsub(
-                                pattern = "'",
-                                replacement = "",
-                                x = from
-                            )
-                        ))
-                    }
-                    return(from)
-                }
-                if (isTRUE(lower)) {
-                    stem <- tolower(stem)
-                }
-                args[["object"]] <- stem
-                stem <- do.call(what = what, args = args)
-                ## Add back extension if necessary. Note that this handles both
-                ## files without an extension and directories in the call.
-                if (!is.na(ext)) {
-                    bn <- paste0(stem, ".", ext)
-                } else {
-                    bn <- stem
-                }
-                to <- file.path(dir, bn)
-                if (identical(from, to)) {
-                    return(from)
-                }
-                if (isFALSE(quiet)) {
-                    AcidCLI::alert(sprintf(
-                        "Renaming {.file %s} to {.file %s}.",
-                        gsub(
-                            pattern = "'",
-                            replacement = "",
-                            x = from
-                        ),
-                        basename(to)
-                    ))
-                }
-                ## nocov start
-                if (isTRUE(insensitive)) {
-                    tmpTo <- file.path(
-                        dirname(from),
-                        paste0(".tmp.", basename(from))
-                    )
-                    ok <- file.rename(from = from, to = tmpTo)
-                    assert(file.exists(tmpTo), ok)
-                    ok <- file.rename(from = tmpTo, to = to)
-                } else {
-                    ok <- file.rename(from = from, to = to)
-                }
-                ## nocov end
-                assert(file.exists(to), ok)
-                to
-            },
+            FUN = toPath,
             FUN.VALUE = character(1L),
             USE.NAMES = FALSE
         )
+        if (isTRUE(dryRun)) {
+            dryRunPath <- function(from, to) {
+                AcidCLI::alertInfo(sprintf(
+                    "[%s] {.file %s} -> {.file %s}.", "dry-run", from, to
+                ))
+            }
+            Map(f = dryRunPath, from = from, to = to, USE.NAMES = FALSE)
+            return(invisible(character()))
+        }
+        renamer <- function(from, to, caseSensitive) {
+            if (identical(from, to)) {
+                return(TRUE)
+            }
+            if (isFALSE(quiet)) {
+                AcidCLI::alert(sprintf(
+                    "Renaming {.file %s} to {.file %s}.", from, to
+                ))
+            }
+            if (isFALSE(caseSensitive)) {
+                tmpTo <- file.path(
+                    dirname(from),
+                    paste0(".tmp.", basename(from))
+                )
+                ok <- file.rename(from = from, to = tmpTo)
+                ok <- file.rename(from = tmpTo, to = to)
+            } else {
+                ok <- file.rename(from = from, to = to)
+            }
+            ok
+        }
+        ok <- unlist(Map(
+            f = renamer,
+            from = from,
+            to = to,
+            MoreArgs = list("caseSensitive" = isFileSystemCaseSensitive()),
+            USE.NAMES = FALSE
+        ))
+        assert(allAreFiles(to), all(ok))
         invisible(to)
     }
