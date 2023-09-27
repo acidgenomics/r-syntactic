88926a38cd9c628160a1b1c8dcf486036eb80f51
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-27 11:39:17 -0400

    Update tests

--- tests/testthat/test-makeWords.R
+++ tests/testthat/test-makeWords.R
@@ -2,7 +2,6 @@ test_that("makeWords", {
     expect_identical(
         object = makeWords(mw),
         expected = c(
-            "kill v. maim",
             "log10 genes per UMI",
             "mito vs. coding",
             "words already",
